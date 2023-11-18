ae03eb159c286cd63977862d4d7a4ecf3046879f
RobinBouchez <rbnbchz@gmail.com>
2023-11-18 18:19:44 +0100

    added user input ish

--- BREAKOUT.asm
+++ BREAKOUT.asm
@@ -15,38 +15,20 @@ SCREEN_HEIGHT equ 200
 SCREEN_WIDTH EQU 320
 VMEMADR EQU 0A0000h
 
+BALL_SIZE EQU 3
+
+CONTROLLER_HEIGHT EQU 3
+CONTROLLER_WIDTH EQU 34
+CONTROLLER_COLOR EQU 9
+
+
 
 ; -------------------------------------------------------------------
 ; CODE
 ; -------------------------------------------------------------------
 CODESEG
 
-; Set the video mode
-PROC setVideoMode
-    ARG     @@VM:byte
-    USES     eax
-
-    movzx ax,[@@VM]
-    int 10h
-
-    ret
-ENDP setVideoMode
-
-PROC processUserInput
-    USES eax, ebx, ecx	
-    mov ecx, KEYCNT
-
-    @@loopkeys:
-    movzx ebx, [byte ptr offset keybscancodes + ecx - 1]	; get scancode
-	mov bl, [offset __keyb_keyboardState + ebx]	; obtain corresponding key state
-	xor ax, ax
-	sub ax, bx	; if key is pressed, AX = FFFF, otherwise AX = 0000
-	loop @@loopkeys
-
-    ret
-ENDP processUserInput
-
-PROC drawRectangle
+PROC draw_rectangle
     ARG     @@x0:word, @@y0:word, @@w:word, @@h:word, @@col: byte
     USES     eax, ecx, edx, edi
 
@@ -82,25 +64,76 @@ PROC drawRectangle
 	rep stosb
 
 	ret
-ENDP drawRectangle
+ENDP draw_rectangle
 
-PROC drawWorld
+PROC draw_controller
+    call draw_rectangle, [controller_x], [controller_y], CONTROLLER_WIDTH, CONTROLLER_HEIGHT, CONTROLLER_COLOR
+    ret
+ENDP draw_controller
+
+PROC move_controller
+
+    push edx
+
+    mov     al, [__keyb_rawScanCode]; last pressed key
+	cmp     al, 1eh ; is a key pressed?
+    jne @@return
+    @@move_controller_right:
+    mov edx, [controller_x] 
+    add edx, 1
+    mov [controller_x], edx
+    
+
+    @@return:
+    pop edx
+
+    ret
+ENDP move_controller
+
+; Set the video mode
+PROC set_video_mode
+    ARG     @@VM:byte
+    USES     eax
+
+    movzx ax,[@@VM]
+    int 10h
+
+    ret
+ENDP set_video_mode
+
+PROC process_user_input
+    USES eax, ebx, ecx	
+    mov ecx, KEYCNT
+
+    @@loopkeys:
+    movzx ebx, [byte ptr offset keybscancodes + ecx - 1]	; get scancode
+	mov bl, [offset __keyb_keyboardState + ebx]	; obtain corresponding key state
+	xor ax, ax
+	sub ax, bx	; if key is pressed, AX = FFFF, otherwise AX = 0000
+	loop @@loopkeys
+    call move_controller
+
+    ret
+ENDP process_user_input
+
+PROC draw_world
+    USES ecx, ebx, edx
     mov ecx, 14
     mov edx, 20
 
 @@outer:
     ;mov ebx, 14
     @@inner:
-        call drawRectangle, edx, [brick_y],BRICK_WIDTH, BRICK_HEIGHT, 2
-        call drawRectangle, edx, [brick_y],BRICK_WIDTH, BRICK_HEIGHT, 2
-        ;call drawRectangle, edx,brick_y,BRICK_WIDTH, BRICK_HEIGHT, 4
-        ;call drawRectangle, edx,brick_y,BRICK_WIDTH, BRICK_HEIGHT, 4
+        call draw_rectangle, edx, [brick_y],BRICK_WIDTH, BRICK_HEIGHT, 2
+        call draw_rectangle, edx, [brick_y],BRICK_WIDTH, BRICK_HEIGHT, 2
+        ;call draw_rectangle, edx,brick_y,BRICK_WIDTH, BRICK_HEIGHT, 4
+        ;call draw_rectangle, edx,brick_y,BRICK_WIDTH, BRICK_HEIGHT, 4
 
-        ;call drawRectangle, edx,brick_y,BRICK_WIDTH, BRICK_HEIGHT, 32
-        ;call drawRectangle, edx,brick_y,BRICK_WIDTH, BRICK_HEIGHT, 32
+        ;call draw_rectangle, edx,brick_y,BRICK_WIDTH, BRICK_HEIGHT, 32
+        ;call draw_rectangle, edx,brick_y,BRICK_WIDTH, BRICK_HEIGHT, 32
     
-        ;call drawRectangle, edx,brick_y,BRICK_WIDTH, BRICK_HEIGHT, 14
-        ;call drawRectangle, edx,brick_y,BRICK_WIDTH, BRICK_HEIGHT, 14
+        ;call draw_rectangle, edx,brick_y,BRICK_WIDTH, BRICK_HEIGHT, 14
+        ;call draw_rectangle, edx,brick_y,BRICK_WIDTH, BRICK_HEIGHT, 14
         add edx, 18
         ;dec ebx
         ;jnz @@inner
@@ -108,21 +141,46 @@ PROC drawWorld
     ;add [brick_y], 10
     loop @@outer
 
-
-    call drawRectangle, [controller_x], [controller_y], 34, 3, 9  ;pallette 9
-    call drawRectangle, SCREEN_WIDTH / 2, SCREEN_HEIGHT - 50, 4, 2, 0fh  ;    ball
+    call draw_controller
+    call draw_rectangle, SCREEN_WIDTH / 2, SCREEN_HEIGHT - 50, 4, 2, 0fh  ;    ball
     
 	ret
-ENDP drawWorld
+ENDP draw_world
+
+PROC update_world
+
+    ret
+ENDP update_world
+
+PROC clearScreenBuffer
+	push	eax
+	push	ecx
+	push	edi
+	push	es
+	
+	cld
+	mov		eax, seg _screenBuffer
+	mov		es, eax
+	mov		edi, offset _screenBuffer
+	mov		ecx, 64000 / 2
+	xor		eax, eax
+	rep		stosw
+	
+	pop		es
+	pop		edi
+	pop		ecx
+	pop		eax
+	ret
+ENDP clearScreenBuffer 
 
 ; Terminate the program.
-PROC terminateProcess  
+PROC terminate_process  
     USES eax
-    call setVideoMode, 03h
-    mov    ax,04C00h
+    call set_video_mode, 03h
+    mov    eax,04C00h
     int 21h
     ret
-ENDP terminateProcess
+ENDP terminate_process
 
 PROC main
      sti            ; set The Interrupt Flag => enable interrupts
@@ -131,18 +189,19 @@ PROC main
     push ds
     pop  es
     
-    call    setVideoMode, 13h
+    call    set_video_mode, 13h
     call __keyb_installKeyboardHandler		
 
-@@mainloop:
-    call drawWorld
-    call processUserInput
-    mov     al, [__keyb_rawScanCode] ; last pressed key
+@@main_loop:
+    call clearScreenBuffer
+    call process_user_input
+    mov     al, [__keyb_rawScanCode]; last pressed key
 	cmp     al, 01h
-    jne @@mainloop
-    ;call updateWorld
-    ;jmp @@mainloop
-    ;@@endofloop:
+    je @@end_of_loop
+    call update_world
+    call draw_world
+    jmp @@main_loop
+    @@end_of_loop:
 
     call __keyb_uninstallKeyboardHandler
 
@@ -150,7 +209,7 @@ PROC main
     mov ah,00h
     int 16h
     
-    call    terminateProcess
+    call    terminate_process
  ENDP main
 	
 
@@ -168,19 +227,12 @@ DATASEG
 
     controller_x dd 140
     controller_y dd 180
+
+    paddle_pos	dw	140	
+	paddle_speed dw	0	
     
     brick_y dd 10
     
-    _plus dw 8, 8 ; W = 8, h = 8
-    db 0, 0, 0, 1, 1, 0, 0, 0
-    db 0, 0, 0, 1, 1, 0, 0, 0
-    db 0, 0, 0, 1, 1, 0, 0, 0
-    db 1, 1, 1, 1, 1, 1, 1, 1
-    db 1, 1, 1, 1, 1, 1, 1, 1
-    db 0, 0, 0, 1, 1, 0, 0, 0
-    db 0, 0, 0, 1, 1, 0, 0, 0
-    db 0, 0, 0, 1, 1, 0, 0, 0
-
     ; scancode values				
 	keybscancodes 	db 29h, 02h, 03h, 04h, 05h, 06h, 07h, 08h, 09h, 0Ah, 0Bh, 0Ch, 0Dh, 0Eh, 	52h, 47h, 49h, 	45h, 35h, 00h, 4Ah
 					db 0Fh, 10h, 11h, 12h, 13h, 14h, 15h, 16h, 17h, 18h, 19h, 1Ah, 1Bh, 		53h, 4Fh, 51h, 	47h, 48h, 49h, 		1Ch, 4Eh
